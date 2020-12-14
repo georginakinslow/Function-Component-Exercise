1540d6b8e8245995fa8b52fbf05c0b819e0e3b2d
Georgina <georginakinslow@yahoo.com>
2020-12-13 22:03:30 -0600

    Completed Final React Exercise

---
+++ ReactApps/final-react-exercise/src/mcushows/MCUShows.js
@@ -0,0 +1,14 @@
+//3a.
+h1 {
+    font-family: Georgia;
+    text-decoration: underline;
+}
+
+//5c. 
+
+.releaseDates {
+<p>WandaVision, Jan 2021</p>
+      <p>The Falcon and the Winter Soldier, March 2021</p>
+      <p>Loki, May 2021</p>
+      <p>Hawkeye, Fall of 2021</p>
+};
